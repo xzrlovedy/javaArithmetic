f7ed4443594bc1ef177b4c7279e9dfe4e31f4e5a
xzrlovedy <3056906329@qq.com>
2019-04-14 10:25:28 +0800

    找出水仙花数

--- ArithmeticExercise/src/arithmeticExercise/Test_01.java
+++ ArithmeticExercise/src/arithmeticExercise/Test_01.java
@@ -1,5 +1,11 @@
 package arithmeticExercise;
-
+/**
+ * 题目：判断101-200之间有多少个素数，并输出所有素数。
+ * 程序分析：判断素数的方法：用一个数分别去除2到sqrt(这个数)，
+ * 如果能被整除， 则表明此数不是素数，反之是素数。
+ * @author Administrator
+ *
+ */
 public class Test_01 {
 	public static void main(String[] args) {
 		int count = 0;
---
+++ ArithmeticExercise/src/arithmeticExercise/Test_02.java
@@ -0,0 +1,20 @@
+package arithmeticExercise;
+/**
+ * 题目：打印出所有的 "水仙花数 "，所谓 "水仙花数 "是指一个三位数，其各位数字立方和等于该数本身。
+ * 例如：153是一个 "水仙花数 "，因为153=1的三次方＋5的三次方＋3的三次方。
+ * @author Administrator
+ *
+ */
+public class Test_02 {
+	public static void main(String[] args) {
+		int a,b,c;
+		for(int i =100;i<1000;i++) {
+			a=i%10;
+			b=i/10%10;
+			c=i/100;
+			if(a*a*a+b*b*b+c*c*c==i) {
+				System.out.println(i);
+			}
+		}
+	}
+}
