08acd7971a1553f10d6373f88a4f78067aad48eb
xzrlovedy <3056906329@qq.com>
2019-07-26 09:49:33 +0800

    两种方式判断一个数是否为完全平方数

--- ArithmeticExercise2/src/com/exercise/Test_01.java
+++ ArithmeticExercise2/src/com/exercise/Test_01.java
@@ -1,5 +1,41 @@
 package com.exercise;
-
+/**
+ * 判断一个数是否为完全平方数，不使用Math库方法
+ * @author Administrator
+ *
+ */
 public class Test_01 {
-
+	/*
+	 * 二分查找 是否存在一个数的平方等于该数字
+	 */
+	public boolean method1(int num) {
+		if(num == 0) {return true;}
+		int left = 1,right = (num < 46340?num:46340),temp = 0;
+		while(left <= right) {
+			temp = left + (right-left)/2;
+			if(num > temp * temp) {
+				left = temp +1;
+			}else if(num == temp*temp) {
+				return true;
+			}else {
+				right = temp-1;
+			}
+		}
+		return false;
+	}
+	/*
+	 * 利用数学知识，找规律
+	 * 1=1
+	 * 4=1+3
+	 * 9=1+3+5
+	 * 16=1+3+5+7
+	 */
+	public boolean method2(int num) {
+		int temp = 1;
+		while(num>0) {
+			num-=temp;
+			temp+=2;
+		}
+		return num == 0;
+	}
 }
