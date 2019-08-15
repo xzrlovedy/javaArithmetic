01d49b69269da448fbdff3031a2c9de586f1fc97
xzrlovedy <3056906329@qq.com>
2019-08-15 09:20:34 +0800

    二叉树的结点

---
+++ BinaryTree/src/binaryTree/Node.java
@@ -0,0 +1,17 @@
+package binaryTree;
+/**
+ * 二叉树的结点
+ * @author Administrator
+ *
+ */
+public class Node {
+	public int data;
+	public Node left;
+	public Node right;
+	
+	public Node(int data) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+}
